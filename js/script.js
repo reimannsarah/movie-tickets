d1e906b22d89aeee845185b50bce068d24750b72
Sarah <reimannsarah45@gmail.com>
2023-04-04 14:15:55 -0700

    defined movie variables and start branch logic for handleFormSubmit
    co-authored-by: Kyle Crawford <kcraw217@gmail.com>
    dquote> co-authored-by: Hernan Verar <hernanverar@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Business Logic
+
 function MovieTheater () {
   this.movies = {};
 }
@@ -22,5 +24,44 @@ Movie.prototype.addTicket = function(ticket) {
 }
 
 MovieTheater.prototype.addMovie = function(movie) {
-  this.movies = movie;
+  this.movies[movie.name] = movie;
 }
+
+let theater = new MovieTheater();
+let cocaineBear = new Movie("Cocaine Bear", ["11am", "1pm", "6pm", "10pm"], true);
+let johnWick = new Movie("John Wick: Chapter 4", ["11am", "1pm", "6pm", "10pm"], true);
+let snatch = new Movie("Snatch", ["11am", "1pm", "6pm", "10pm"], false);
+let avatar = new Movie("Avatar: Way of Water", ["11am", "1pm", "6pm", "10pm"], false);
+let ticket = new Ticket(0,5,8,4);
+let movieArray = [cocaineBear, johnWick, snatch, avatar];
+
+movieArray.map(function(movie){
+  movie.addTicket(ticket);
+  theater.addMovie(movie);  
+});
+
+
+
+
+// UI Logic
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  let movie = document.querySelector("#movie-options").value;
+  let showtime = document.querySelector("#showtime-options").value;
+  let ticketChoice = document.querySelector("#ticket-options").value.toLowerCase();
+
+  const ticketKeys = Object.keys(ticket);
+  ticketKeys.forEach(key){
+    if(key === ticketChoice){
+      
+    }
+  }
+}
+
+
+
+
+window.addEventListener("load", function() {
+  this.document.querySelector("form").addEventListener("submit", handleFormSubmit);
+});
