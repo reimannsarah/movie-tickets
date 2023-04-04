501ba575d4a1b0ea79b2842683ff33c85522412a
Sarah <reimannsarah45@gmail.com>
2023-04-04 09:48:56 -0700

    pass test 2
    co-authored-by: Kyle Crawford <kcraw217@gmail.com>
    dquote> co-authored-by: Hernan Verar <hernanverar@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 function MovieTheater () {
   this.movies = {};
 }
+
+function Movie (name, showtimes, firstRelease, minAge) {
+  this.name = name;
+  this.showtimes = showtimes;
+  this.firstRelease = firstRelease;
+  this.minAge = minAge;
+}
